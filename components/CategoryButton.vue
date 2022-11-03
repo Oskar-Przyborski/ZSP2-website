10a8107915dff5e7bcca1c84634a78f124b09522
Oskar-Przyborski <oskar.przski@gmail.com>
2022-11-03 19:21:11 +0100

    Added Categories and szkola page

---
+++ components/CategoryButton.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+interface Props {
+    title: string,
+    description: string,
+    path?: string
+}
+defineProps<Props>()
+</script>
+<template>
+    <NuxtLink :to="path" class="category-button">
+        <div class="text">
+            <div class="title">{{ title }}</div>
+            <div class="description">{{ description }}</div>
+        </div>
+        <div class="arrow">
+            <Icon name="mdi:arrow-right" size="25" />
+        </div>
+    </NuxtLink>
+</template>
+<style lang="scss" scoped>
+.category-button {
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    text-decoration: none;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .text {
+        padding: 1em;
+        padding-right: 1.5em;
+
+        .title {
+            font-size: 1.3em;
+            font-weight: 500;
+        }
+
+        .description {
+            padding-top: .7em;
+            color: #252525;
+        }
+    }
+
+    .arrow {
+        height: 100%;
+        aspect-ratio: 1/1;
+        position: relative;
+        display: grid;
+        place-items: center;
+
+        &::before {
+            position: absolute;
+            content: "";
+            top: 0;
+            right: 0;
+            bottom: 0;
+            background-color: var(--yellow);
+            z-index: -1;
+            width: 0%;
+            transition: width 0.2s ease-in-out;
+        }
+    }
+
+
+
+    &:hover {
+        .arrow {
+            &::before {
+                width: 100%;
+            }
+        }
+    }
+}
+</style>
---
+++ pages/szkola.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Szkoła</h1>
+        <div class="categories-grid">
+            <CategoryButton title="O Nas" description="Co Robimy? Kim jesteśmy?" path="/szkola/o-nas" />
+            <CategoryButton title="Podręczniki" description="Wykaz potrzebnych Ci podręczników"
+                path="/szkola/podreczniki" />
+            <CategoryButton title="RSU" description="Nasz skład i plany" path="/szkola/rsu" />
+            <CategoryButton title="Konkursy" description="Chiałbyś coś wygrać? Zajrzyj!" path="/szkola/konkursy" />
+            <CategoryButton title="Sport" description="Ruch to zdrowie" path="/szkola/sport" />
+            <CategoryButton title="Uczelnie współorganizujące" description="Co Robimy? Kim jesteśmy?"
+                path="/szkola/uczelnie-wspolorganizujace" />
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.categories-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 15px;
+}
+</style>
