67b08e416bf3e01e7ace8db3945a19a7f67ef68f
Oskar-Przyborski <oskar.przski@gmail.com>
2022-11-12 11:57:40 +0100

    articles sorting by datetime + formatting

--- app/composables/getArticles.ts
+++ app/composables/getArticles.ts
@@ -1,6 +1,6 @@
 export const getArticles = async () => {
 	const sanity = useSanity();
-	const query = groq`*[_type=="article"]{
+	const query = groq`*[_type=="article"] | order(datetime) {
         title,
         "slug": slug.current,
         "imageUrl": image.asset -> url,
@@ -10,16 +10,19 @@ export const getArticles = async () => {
 	return await sanity.fetch<Article[]>(query);
 };
 
-export const getPreviewFromArticle = (article: Article, maxLenght: number): string => {
+export const getPreviewFromArticle = (
+	article: Article,
+	maxLenght: number
+): string => {
 	const firstArticle = article.body.find((x) => x._type == "block");
 	let preview = "";
-    for(const child of firstArticle.children){
-        preview = preview.concat(child.text)
-        if(preview.length>maxLenght-3){
-            preview = preview.substring(0,maxLenght-3)
-            preview = preview + "..."
-            break;
-        }
-    }
+	for (const child of firstArticle.children) {
+		preview = preview.concat(child.text);
+		if (preview.length > maxLenght - 3) {
+			preview = preview.substring(0, maxLenght - 3);
+			preview = preview + "...";
+			break;
+		}
+	}
 	return preview;
 };
