c4bcb946af9f7e9be7dc2037a27af42c5e20060b
Oskar-Przyborski <oskar.przski@gmail.com>
2022-11-12 18:20:29 +0100

    dodano datę w artykułach

--- app/pages/aktualnosci/[slug].vue
+++ app/pages/aktualnosci/[slug].vue
@@ -5,16 +5,24 @@ const article = await getArticle(route.params["slug"] as string)
 <template>
     <BackButton to="/aktualnosci">Aktualności</BackButton>
     <article>
-        <h1>{{ article.title }}</h1>
+        <div class="date">{{ getDateFromArticle(article) }}</div>
+        <h1 class="title">{{ article.title }}</h1>
         <NuxtImg v-if="article.imageUrl != null" :src="article.imageUrl" class="img-fluid img" />
         <SanityContent :blocks="article.body" />
     </article>
 </template>
 <style lang="scss" scoped>
 article {
-    font-size: 1.15em;
-    padding: .5em 1em;
+    font-size: 1.05em;
+    padding: 1em 1.2em;
     border: 1px solid rgba(0, 0, 0, 0.125);
+
+    .date{
+        font-size: 0.9em;
+    }
+    .title{
+        margin: 0.6em 0;
+    }
 
     img {
         display: block;
