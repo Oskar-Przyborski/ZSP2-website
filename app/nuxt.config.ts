1b1c77efeeeed8adfbd9f077111ae63c864d5247
Squar <oskar.przski@gmail.com>
2024-11-16 15:38:19 +0100

    update deps, remove vercel analytics, add robots.txt

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,45 +1,53 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/sanity", "nuxt-icon", "@nuxt/image-edge"],
-	plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
+	compatibilityDate: "2024-11-16",
+	modules: ["@nuxtjs/sanity", "@nuxt/icon", "@nuxt/image-edge"],
 	css: ["~/assets/global.css"],
 	sanity: {
 		projectId: "vm3xce09",
-		dataset:
-			process.env.ENV == "DEVELOPMENT" ? "development" : "production",
-		useCdn: true,
+		dataset: process.env.ENV == "DEVELOPMENT" ? "development" : "production",
+		useCdn: true
 	},
 	app: {
 		head: {
 			htmlAttrs: {
-				lang: "pl",
+				lang: "pl"
 			},
 			link: [
 				{
 					rel: "icon",
 					sizes: "192x192",
-					href: "/Logo192x192.webp",
+					href: "/Logo192x192.webp"
 				},
 				{
 					rel: "icon",
 					sizes: "32x32",
-					href: "/Logo32x32.webp",
+					href: "/Logo32x32.webp"
 				},
 				{
 					rel: "apple-touch-icon",
 					sizes: "32x32",
-					href: "/Logo32x32.webp",
+					href: "/Logo32x32.webp"
 				},
 				{
 					rel: "preconnect",
-					href: "https://fonts.gstatic.com",
+					href: "https://fonts.gstatic.com"
 				},
 				{
 					rel: "preconnect",
-					href: "https://fonts.googleapis.com",
-				},
-			],
-		},
+					href: "https://fonts.googleapis.com"
+				}
+			]
+		}
 	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: 'modern-compiler'
+				}
+			}
+		}
+	}
 });
